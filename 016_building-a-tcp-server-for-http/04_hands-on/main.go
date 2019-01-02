199c2a4a2259326c5a111edd98bfc88dabdced3a
Tai-Lin Wu <Tai-Lin.Wu@ibm.com>
2019-01-01 22:56:37 -0500

    Add solution to 017/03

---
+++ 016_building-a-tcp-server-for-http/04_hands-on/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Fatalln(err.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+
+	// read request
+	method, url := request(conn)
+
+	// write response
+	respond(conn, method, url)
+}
+
+func request(conn net.Conn) (string, string) {
+	i := 0
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// request line
+			m := strings.Fields(ln)[0]
+			u := strings.Fields(ln)[1]
+			return m, u
+		}
+		if ln == "" {
+			// headers are done
+			break
+		}
+		i++
+	}
+	return "", ""
+}
+
+func respond(conn net.Conn, m string, u string) {
+	body := ""
+	if u == "/yes" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>YES SIR</strong></body></html>`
+
+	} else if u == "/no" {
+		body = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>NO SIR</strong></body></html>`
+	} else {
+		body = fmt.Sprintf(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title></title></head><body><strong>You are typing: %s</strong></body></html>`, u)
+	}
+	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/html\r\n")
+	fmt.Fprint(conn, "\r\n")
+	fmt.Fprint(conn, body)
+}
